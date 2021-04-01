69f83d63bf47533a3094ff85ced13ece215f5ba6
Yaxue Guo <yaxue.guo23@gmail.com>
2021-04-01 14:27:13 -0400

    #61: added static content to publications page

--- src/pages/resources/publications.js
+++ src/pages/resources/publications.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AnimateOnMount } from '../../components/anim'
 import { SEO } from '../../components/seo'
-import { Title, Paragraph } from '../../components/typography'
+import { Title, Paragraph, Subheading } from '../../components/typography'
 
 const PublicationsPage = () => (
     <AnimateOnMount>
@@ -10,7 +10,18 @@ const PublicationsPage = () => (
         <Title>Publications</Title>
 
         <Paragraph>
-            Coming soon.
+            <h3>
+              <a href="https://ieeexplore.ieee.org/document/8972790" target="_blank" rel="noreferrer">
+                FABRIC: A National-Scale Programmable Experimental Network Infrastructure
+              </a>
+            </h3>
+            <p>Ilya Baldin, Anita Nikolich, James Griffioen, Indermohan Inder S. Monga, Kuang-Ching Wang, Tom Lehman, Paul Ruth<br></br>
+            DOI: <a href="10.1109/MIC.2019.2958545" target="_blank" rel="noreferrer">10.1109/MIC.2019.2958545</a>
+            </p>
+            <b>Abstract:</b>
+            <p>
+              FABRIC is a unique national research infrastructure to enable cutting-edge and exploratory research at-scale in networking, cybersecurity, distributed computing and storage systems, machine learning, and science applications. It is an everywhere-programmable nationwide instrument comprised of novel extensible network elements equipped with large amounts of compute and storage, interconnected by high speed, dedicated optical links. It will connect a number of specialized testbeds for cloud research (NSF Cloud testbeds CloudLab and Chameleon), for research beyond 5G technologies (Platforms for Advanced Wireless Research or PAWR), as well as production high-performance computing facilities and science instruments to create a rich fabric for a wide variety of experimental activities.
+            </p>
         </Paragraph>
     </AnimateOnMount>
 
