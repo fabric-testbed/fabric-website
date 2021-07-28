e57dc96f2cd086ddcddf16ae3444f02b524da1b0
Yaxue Guo <37635744+yaxue1123@users.noreply.github.com>
2021-07-28 17:06:00 -0400

    Content/125 update menu and about page (#127)
    * feature-new-leadership-page (#3)
    
    * add sample data and images for leadership page
    
    * add route/ menu for new leadership page
    
    * add leadership page and hook
    
    * add complete markdown data and images for new leadership page
    
    * added the filed of order to data & sort leaders in hook
    
    * fix menu dropdown item hover color issue & add key to sac/ leadership items
    
    * update Anit'a institution and corresponding logo in partners list
    
    add u of i logo
    
    update institution
    
    remove iit logo
    
    pull in new u of i logo for partners module
    
    * Link RedHat Research Days in Event (#7)
    
    * update Anit'a institution and corresponding logo in partners list (#5)
    
    add u of i logo
    
    update institution
    
    remove iit logo
    
    pull in new u of i logo for partners module
    
    * Link the RedHat Research days event
    
    * #51: updated event content (#52)
    
    * Enhancement/112 update event template for presenter (#117)
    
    * #112: added optional presenter field to event template
    
    * #112: added presenter link field to event template
    
    * Content/113 and 122: updated get involved page and updated top navigation bar (#123)
    
    * Enhancement/112 116 119 add code sign (#121)
    
    * #116: added news article
    
    * #116: updated event template to include presenter and presetation link
    
    * #116: added event for US Networking workshop for Open call 4
    
    * #122: made Resources->Documentation point to FABRIC knowledge base website
    
    * #122: added portal item to navbar
    
    * #122: separated Get Involved page into three pages
    
    * #122: replaced old /get-involved path with new links
    
    * #122: made 'Portal' item in menu bar stand out by color and font weight
    
    * fixed event template
    
    * Content/125 update menu and about page (#126)
    
    * #125: updated resources submenu
    
    * #125: moved what's fabric presentation and webinar to about page
    
    * #125: removed faq and design doc pages completely
    
    Co-authored-by: Matt Watson <mattwat@gmail.com>

--- src/pages/about/overview.js
+++ src/pages/about/overview.js
@@ -5,6 +5,7 @@ import { AnimateOnMount } from '../../components/anim'
 import { SEO } from '../../components/seo'
 import { Title, Subheading, Paragraph } from '../../components/typography'
 import { Module } from '../../components/layout'
+import { Container as Grid, Row, Col } from 'react-grid-system'
 import { ButtonLink } from '../../components/button'
 import { useCapabilities } from '../../hooks'
 
@@ -49,6 +50,21 @@ const CapabilityBody = styled.div`
     margin: 0 0 0 0;
 `
 
+const ResponsiveVideoContainer = styled.div`
+    overflow: hidden;
+    padding-bottom: 56.25%;
+    margin-bottom: 2rem;
+    position: relative;
+    height: 0;
+    & iframe {
+        left: 0;
+        top: 0;
+        height: 100%;
+        width: 100%;
+        position: absolute;
+    }
+`
+
 const AboutPage = () => {
     const capabilities = useCapabilities()
 
@@ -91,6 +107,31 @@ const AboutPage = () => {
                 }
             </Module>
  
+            <Module title="What is FABRIC?">
+                <Paragraph>
+                    Below are two resources&mdash;a presentation and a webinar&mdash;that
+                    may provide further information to assist with answering your FABRIC questions.
+                </Paragraph>
+
+                <Grid fluid>
+
+                    <Row>
+                        <Col xs={ 12 } md={ 6 }>
+                            <Subheading>Presentation</Subheading>
+                            <ResponsiveVideoContainer>
+                                <iframe title="Presentation: What is FABRIC?" src="https://drive.google.com/file/d/1Wa8kkuyycSBRNjUZIVYXFbSRHt2n4Vhy/preview" width="100%" height="600"></iframe>
+                            </ResponsiveVideoContainer>
+                        </Col>
+                        <Col xs={ 12 } md={ 6 }>
+                            <Subheading>Webinar</Subheading>
+                            <ResponsiveVideoContainer>
+                               <iframe title="Webinar: What is FABRIC?" width="948" height="518" src="https://www.youtube.com/embed/ofLz_7rWTDg" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                            </ResponsiveVideoContainer>
+                        </Col>
+                    </Row>
+                </Grid>
+            </Module>
+
             
             <Paragraph center style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <ButtonLink to="/resources/brochures" secondary>Download FABRIC Brochures</ButtonLink>
