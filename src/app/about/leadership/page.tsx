c4aa3fb9ebb7e0581ee6fd4b482388dd6cab80d7
yaxue1123 <yaxue.guo23@gmail.com>
2026-08-24 14:09:21 -0400

    Update Leadership page

--- src/app/about/leadership/page.tsx
+++ src/app/about/leadership/page.tsx
@@ -43,12 +43,7 @@ const leaders = [
     name: "Dale Carder",
     photo: "/imgs/leadership/dale-carder.jpg",
     bio: "Dale Carder is a Network Engineer at Lawrence Berkeley National Laboratory working on ESnet. He builds large-scale, complex, resilient, next-generation long-haul networks for International science projects such as the Large Hadron Collider (LHC). His expertise lies in the areas of dynamic provisioning, global traffic engineering and control frameworks. He comfortably works at the intersection of software, network, compute, application workflows, and systems engineering and has run R&E networks for 20 years.",
-  },
-  {
-    name: "Bryttany Todd",
-    photo: "/imgs/leadership/bryttany-todd.jpg",
-    bio: "Bryttany Todd is the Deputy Director of Research Operations at RENCI, UNC-Chapel Hill. Her interests include developing and executing project and program management strategy and operational needs. She currently manages various research projects and RENCI programs including FABRIC, MERIF, and RENCI's internship program.",
-  },
+  }
 ];
 
 const pastCollaborators = [
